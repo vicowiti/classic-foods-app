325b8f4bf8811f7097397bbac763e672b43bf36c
Victor Owiti <viowiti12@gmail.com>
2022-09-22 14:22:37 +0300

    Added the ui for the register and login pages:pending functionality

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
 const Login = () => {
-  return <section>Login</section>;
+  return (
+    <section className="relative w-full">
+      <article className="mt-48 mb-32  w-[80vw] rounded-xl flex flex-col justify-center items-center mx-auto">
+        <div>
+          <h2 className="font-bold text-3xl">Login</h2>
+        </div>
+        <form className="mt-7 flex flex-col gap-5">
+          <input
+            type="email"
+            className="p-3 rounded-sm bg-yellow-400/30 outline-none"
+            placeholder="Enter Email"
+          />
+          <input
+            type="password"
+            className="p-3 rounded-sm bg-yellow-400/30 outline-none"
+            placeholder="Password"
+          />
+
+          <button
+            type="submit"
+            className=" text-lg p-3 w-full bg-orange-500 rounded-xl"
+          >
+            Order
+          </button>
+        </form>
+      </article>
+    </section>
+  );
 };
 
 export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,54 @@
 import React from "react";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <section className="relative w-full">
+      <article className="mt-44 mb-32  w-[80vw] rounded-xl flex flex-col justify-center items-center mx-auto">
+        <div>
+          <h2 className="font-bold text-3xl">Register</h2>
+        </div>
+        <form className="mt-7 flex flex-col gap-5 w-[80%] md:w-[50%]">
+          <input
+            type="text"
+            required
+            className="p-3 rounded-sm bg-yellow-400/30 outline-none"
+            placeholder="Enter Name"
+          />
+          <input
+            type="email"
+            required
+            className="p-3 rounded-sm bg-yellow-400/30 outline-none"
+            placeholder="Enter Email"
+          />
+          <input
+            type="number"
+            required
+            className="p-3 rounded-sm bg-yellow-400/30 outline-none"
+            placeholder="Enter Contact"
+          />
+          <input
+            required
+            type="password"
+            className="p-3 rounded-sm bg-yellow-400/30 outline-none"
+            placeholder="Password"
+          />
+          <input
+            required
+            type="password"
+            className="p-3 rounded-sm bg-yellow-400/30 outline-none"
+            placeholder="Repeat Password"
+          />
+
+          <button
+            type="submit"
+            className=" text-lg p-3 w-full bg-orange-500 rounded-xl"
+          >
+            Add Me
+          </button>
+        </form>
+      </article>
+    </section>
+  );
 };
 
 export default Register;
