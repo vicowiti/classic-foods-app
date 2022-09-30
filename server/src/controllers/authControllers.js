b47a3069a3eb27e399cf8745746836bcff6841d9
Victor Owiti <viowiti12@gmail.com>
2022-09-30 09:33:14 +0300

    added login logic on the backend

--- server/src/controllers/authControllers.js
+++ server/src/controllers/authControllers.js
@@ -44,7 +44,39 @@ const regController = asyncHandler(async (req, res) => {
   }
 });
 
-const authController = asyncHandler(async (req, res) => {});
+// Login Logic
+const authController = asyncHandler(async (req, res) => {
+  // Check if both input were submitted
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Kindly provide required input");
+  }
+
+  //Check if theres a user of similar email.
+
+  const existingUser = await User.findOne({ email });
+
+  if (!existingUser) {
+    res.status(404);
+    throw new Error("User does not exist, kindly sign up");
+  }
+
+  // If available, check validity of password.
+
+  if (existingUser && (await bcrypt.compare(password, existingUser.password))) {
+    res.status(200).json({
+      name: existingUser.name,
+      _id: existingUser.id,
+      email: existingUser.email,
+      token: generateJWT(existingUser.id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid credentials");
+  }
+  // Return the user object with an accompanying JWT to be stored in the frontend
+});
 
 module.exports = {
   regController,
